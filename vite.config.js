63f6ac400e94eb2f606c1bc1b8087476a51e2642
shubham <shubhamsingh07854@gmail.com>
2025-05-13 20:50:12 +0530

    Updated Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/emp-mgmt-portal/', // Important: Use your GitHub repo name here
+  base: '/Emp-MGMT-Portal/', // 👈 Add this line
 });
